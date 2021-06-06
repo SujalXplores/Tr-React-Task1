e85d7a24e8d292a5740c7101467e13cb3a89d08e
SujalShah3234 <shahc9437@gmail.com>
2021-06-06 22:08:02 +0530

    ✨ Tabular layout with responsive ui

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -1,25 +1,53 @@
 import React from "react";
 import Card from "../UI/Card";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
 import styles from "./UserList.module.css";
 
 const UserList = (props) => {
-  console.log(props);
   if (props.users.length > 0) {
     return (
       <Card>
-        <h2>Records:</h2>
-        <ul className={styles.ul}>
-          {props.users.map((user) => (
-            <li
-              key={user.id}
-              className={styles.li}
-              onClick={() => props.onDelete(user.id)}
-            >
-              {user.username}, Age: {user.age},{user.firstname}, {user.lastname}
-              ,{user.maths},{user.science}
-            </li>
-          ))}
-        </ul>
+        <TableContainer component={Paper}>
+          <Table className={styles.user__table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell>Last Name</TableCell>
+                <TableCell>Username</TableCell>
+                <TableCell>Age</TableCell>
+                <TableCell>Maths</TableCell>
+                <TableCell>Science</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {props.users.map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell>{user.username}</TableCell>
+                  <TableCell>{user.age}</TableCell>
+                  <TableCell>{user.firstname}</TableCell>
+                  <TableCell>{user.lastname}</TableCell>
+                  <TableCell>{user.maths}</TableCell>
+                  <TableCell>{user.science}</TableCell>
+                  <TableCell>
+                    <IconButton color="secondary" onClick={() => props.onDelete(user.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Card>
     );
   }
