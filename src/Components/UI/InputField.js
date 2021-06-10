21427f5f549cd85febd512eb867e8180d5e698af
SujalShah3234 <shahc9437@gmail.com>
2021-06-10 16:06:45 +0530

    🐛 input label comes down bug fixed

--- src/Components/UI/InputField.js
+++ src/Components/UI/InputField.js
@@ -4,18 +4,7 @@ import styles from "./InputField.module.css";
 export default function InputField(props) {
   return (
     <div className={styles.input__wrap}>
-      <input
-        id={props.id}
-        name={props.name}
-        onChange={props.onChange}
-        value={props.value}
-        required={props.required}
-        type={props.type}
-        min={props.min}
-        max={props.max}
-        disabled={props.disabled}
-        className={styles.main__input}
-      />
+      <input {...props} placeholder=" " className={styles.main__input} />
       <label className={styles.input__label}>
         {props.label}
         {props.required ? " *" : ""}
